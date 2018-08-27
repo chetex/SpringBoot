0f4354d46fc5bedfcd818a0e7685b135167a65ea
chetex <chetex69@gmail.com>
2018-08-27 21:41:44 +0200

    New controller

--- 3N-Spring-Boot-Rest/initial/src/main/java/hello/GreetingController.java
+++ 3N-Spring-Boot-Rest/initial/src/main/java/hello/GreetingController.java
@@ -5,6 +5,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import bye.Bye;
+
 /* This code uses Spring 4’s new @RestController annotation, 
  * which marks the class as a controller where every method returns a domain object instead of a view. 
  * It’s shorthand for @Controller and @ResponseBody rolled together.
@@ -12,11 +14,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class GreetingController {
 
-    private static final String TEMPLATE = "Hello, %s!";
+    private static final String TEMPLATE_HELLO = "Hello, %s!";
+    private static final String TEMPLATE_BYE = "Bye, %s!";
     private final AtomicLong counter = new AtomicLong();
 
     @RequestMapping("/greeting")
     public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
-        return new Greeting(counter.incrementAndGet(), String.format(TEMPLATE, name));
+        return new Greeting(counter.incrementAndGet(), String.format(TEMPLATE_HELLO, name));
+    }
+    
+    @RequestMapping("/bye")
+    public Bye bye(@RequestParam(value="name", defaultValue="World") String name) {
+        return new Bye(counter.incrementAndGet(), String.format(TEMPLATE_BYE, name));
     }
 }
