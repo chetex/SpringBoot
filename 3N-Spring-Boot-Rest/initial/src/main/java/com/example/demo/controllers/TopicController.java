1ad14e604412bdc48afad8c6ec8d7a3a8661a8f0
chetex <chetex69@gmail.com>
2018-09-01 16:20:54 +0200

    add new request mapping, and object to covert in JSON

--- 3N-Spring-Boot-Rest/initial/src/main/java/com/example/demo/controllers/TopicController.java
+++ 3N-Spring-Boot-Rest/initial/src/main/java/com/example/demo/controllers/TopicController.java
@@ -1,5 +1,8 @@
 package com.example.demo.controllers;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,7 +10,9 @@ import org.springframework.web.bind.annotation.RestController;
 public class TopicController {
 	
 	@RequestMapping("/topics")
-	public String getAllTopics () {
-		return "All topics response";
+	public List<Topic> getAllTopics () {
+		return Arrays.asList(
+				new Topic(0, "nacho", "Creador"), 
+				new Topic(1, "pepe", "ddd"));
 	}
 }
