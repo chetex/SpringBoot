a65d1caa9d727967b2e9bc385799ad3f8abce6ce
chetex <chetex69@gmail.com>
2018-09-07 08:01:09 +0200

    New manual project

---
+++ 4n-spring-boot-jpa-rest/src/main/java/com/examplejpa/objects/Car.java
@@ -0,0 +1,50 @@
+package com.examplejpa.objects;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Car {
+	
+	@Id
+	private int id;
+	private String name;
+	private String brand;
+	
+	/**
+	 * @return the id
+	 */
+	public int getId() {
+		return id;
+	}
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(int id) {
+		this.id = id;
+	}
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+	/**
+	 * @return the brand
+	 */
+	public String getBrand() {
+		return brand;
+	}
+	/**
+	 * @param brand the brand to set
+	 */
+	public void setBrand(String brand) {
+		this.brand = brand;
+	}
+}
