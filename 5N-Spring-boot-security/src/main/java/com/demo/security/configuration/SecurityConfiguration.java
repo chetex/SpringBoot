7c6703f727801a86ebbd3400d1f3afc8cbe5e2b0
chetex <chetex69@gmail.com>
2018-09-07 12:18:10 +0200

    Adding user and password authenticatio

--- 5N-Spring-boot-security/src/main/java/com/demo/security/configuration/SecurityConfiguration.java
+++ 5N-Spring-boot-security/src/main/java/com/demo/security/configuration/SecurityConfiguration.java
@@ -1,5 +1,8 @@
 package com.demo.security.configuration;
 
+import javax.servlet.Filter;
+
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -23,17 +26,26 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 			// Authoriza all my request | 
 			httpSecurity
 				.authorizeRequests()
-				// .antMatchers("**/rest/*")
+//				.antMatchers("*/hello").hasRole("USER")
+//				.antMatchers("**/rest/*")
 				.anyRequest()
-				//.permitAll() // Not request authentication user and password
 				.fullyAuthenticated()
-				.and().httpBasic();
+//				.permitAll() // Not request authentication user and password
+				.and()
+//				.addFilterBefore(customFilter(), BasicAuthenticationFilter.class)
+//				.fullyAuthenticated()
+				.httpBasic();
 			
-			// Disabe 
+			// Disabe cross site 
 			httpSecurity.csrf().disable();
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+	}
+
+	@Bean
+	public Filter customFilter() {
+		return new CustomFilter();
 	}
 }
