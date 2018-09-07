c41f60284a537d560e9fd37c39f99f0c1dd8221f
chetex <chetex69@gmail.com>
2018-09-07 10:55:15 +0200

    Adding user and password authentication

--- 5N-Spring-boot-security/src/main/java/com/demo/security/configuration/SecurityConfiguration.java
+++ 5N-Spring-boot-security/src/main/java/com/demo/security/configuration/SecurityConfiguration.java
@@ -1,6 +1,7 @@
 package com.demo.security.configuration;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -10,15 +11,22 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 	
 	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.inMemoryAuthentication()
+			.withUser("ajay").password("{noop}test").roles("USER").and()
+			.withUser("demo").password("{noop}test2").roles("ADMIN");
+	}
+	
+	@Override
 	public void configure(HttpSecurity httpSecurity) {
 		try {
-			
 			// Authoriza all my request | 
 			httpSecurity
 				.authorizeRequests()
 				// .antMatchers("**/rest/*")
 				.anyRequest()
-				.permitAll()
+				//.permitAll() // Not request authentication user and password
+				.fullyAuthenticated()
 				.and().httpBasic();
 			
 			// Disabe 
