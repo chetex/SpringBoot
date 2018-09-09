5d5c36faceb7b3953b3aac3daf04d93cce747078
chetex <chetex69@gmail.com>
2018-09-09 16:57:14 +0200

    ffffff

--- 4n-spring-boot-jpa-rest/src/main/java/com/examplejpa/controller/CarControllerInterface.java
+++ 4n-spring-boot-jpa-rest/src/main/java/com/examplejpa/controller/CarControllerInterface.java
@@ -2,13 +2,11 @@ package com.examplejpa.controller;
 
 import java.util.List;
 
-import org.springframework.web.bind.annotation.PathVariable;
-
 import com.examplejpa.objects.Car;
 
 public interface CarControllerInterface {
 	public List<Car> getAllCars();
-	public Car getSpecificCar(@PathVariable int id);
+	public Car getSpecificCar(int id);
 	public void addCar(Car car);
 	public void updateCar(Car car);
 	public void deleteCar(Car car);
