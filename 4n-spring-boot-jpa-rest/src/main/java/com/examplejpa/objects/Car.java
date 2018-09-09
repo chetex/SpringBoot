6336de69355a6cd394250d52595a7dfc23134bcb
chetex <chetex69@gmail.com>
2018-09-09 16:57:29 +0200

    ffff

--- 4n-spring-boot-jpa-rest/src/main/java/com/examplejpa/objects/Car.java
+++ 4n-spring-boot-jpa-rest/src/main/java/com/examplejpa/objects/Car.java
@@ -2,6 +2,8 @@ package com.examplejpa.objects;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
 
 @Entity
 public class Car {
@@ -10,6 +12,22 @@ public class Car {
 	private int id;
 	private String name;
 	private String brand;
+	private int buyDate;
+	
+	@OneToOne
+	private Dashboard dashBoard;
+	
+	@OneToMany
+	private CarReview carReviews;
+	
+	public enum GAS_TYPE {
+		DIESEL,
+		GASOLINA
+	}
+	
+	private String power;
+	private String color;
+	
 	
 	/**
 	 * @return the id
@@ -46,5 +64,41 @@ public class Car {
 	 */
 	public void setBrand(String brand) {
 		this.brand = brand;
+	}
+	/**
+	 * @return the power
+	 */
+	public String getPower() {
+		return power;
+	}
+	/**
+	 * @param power the power to set
+	 */
+	public void setPower(String power) {
+		this.power = power;
+	}
+	/**
+	 * @return the buyDate
+	 */
+	public int getBuyDate() {
+		return buyDate;
+	}
+	/**
+	 * @param buyDate the buyDate to set
+	 */
+	public void setBuyDate(int buyDate) {
+		this.buyDate = buyDate;
+	}
+	/**
+	 * @return the color
+	 */
+	public String getColor() {
+		return color;
+	}
+	/**
+	 * @param color the color to set
+	 */
+	public void setColor(String color) {
+		this.color = color;
 	}
 }
